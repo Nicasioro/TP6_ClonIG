ea4f32fb6c138e90e72c56ac074803a0cb504fee
NICOLAS DARREN NAVARRO FEINBERG <48795403@ort>
2026-06-16 14:50:33 -0300

    fbiubi

--- mi-proyecto/src/data/user.js
+++ mi-proyecto/src/data/user.js
@@ -1,7 +1,7 @@
 const user = {
   username: "michi_master",
   name: "Nicolas Navarro",
-  bio: "Fanático de los gatos y React 🐱",
+  bio: "Sí, sabes que ya llevo un rato mirándote tengo que bailar contigo hoy vi que tu mirada ya estaba llamándome muéstrame el camino que yo voy",
   followers: 1200,
   following: 450,
   posts: 10,
